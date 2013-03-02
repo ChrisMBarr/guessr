8d8e455bd0e48cbc569216eec64f4dbda0d15eac
Chris Barr <chris.m.barr@gmail.com>
2013-03-02 00:18:43 -0600

    Updated with new jQuery version, no DB, words in categories, and a hint halfway through the game

---
+++ js/script.js
@@ -0,0 +1,233 @@
+var $Game = function($){
+	//Vars
+	var currentTag;
+	var guessedTag;
+	var numGuesses=0;
+
+	var settings={
+		imagesToDisplay: 20,
+		gameDuration: 30,
+		animationSpeed: 200,
+		easing: 'easeOutExpo'
+	};
+	settings.dropAnimationOpts = {
+		duration:600,
+		easing:settings.easing,
+		direction:"up"
+	};
+	
+
+	//Init
+	$(function(){
+		setup.buttons();
+		setup.dialogs();
+
+		//initial page load animation
+		$("#intro").show("drop",settings.dropAnimationOpts);
+	});
+
+	var setup={
+		buttons:function(){
+			//Start the first game & hide instructions
+			$("#startFirstGame").click(actions.startFirstGame);
+
+			//Take a guess
+			$("#guess").submit(function(){
+				actions.guess();
+				return false;
+			});
+			//manually start a new game
+			$("#newTag").click(function(){
+				actions.startGame();
+				return false;
+			});
+			//Give up a game (lose)
+			$("#giveUp").click(actions.loseGame);
+
+			//button hover effects
+			$(".button").hover(function(){ 
+				$(this).addClass("ui-state-hover"); 
+			},function(){ 
+				$(this).removeClass("ui-state-hover"); 
+			});
+		},
+		dialogs:function(){
+			//Winning Dialog Box
+			$("#winDialog").dialog({
+				bgiframe: true,
+				modal: true,
+				autoOpen: false,
+				width:400,
+				buttons: {
+					"Cool": function() {
+						$(this).dialog('close');
+						actions.startGame();
+					}
+				}
+			});
+			//Losing Dialog Box
+			$("#loseDialog").dialog({
+				bgiframe: true,
+				modal: true,
+				autoOpen: false,
+				width:400,
+				buttons: {
+					"I am bad at this game": function() {
+						$(this).dialog('close');
+						actions.startGame();
+					}
+				}
+			});
+		}
+	}
+
+	var actions = {
+		startFirstGame:function(){
+			$("#intro").hide("drop",settings.dropAnimationOpts,function(){
+				$("#gameOptions").show("drop",settings.dropAnimationOpts,function(){
+					$("#guess").show("drop",settings.dropAnimationOpts, actions.startGame);
+				});
+			});
+			return false;
+		},
+		startGame:function(){
+			timer.stop();
+
+			$("#hint").hide();
+			$("#guesses, #photos,#preloadPhotos").empty();
+			$("<li class='clear'></li>").appendTo("#guesses");
+			$("#gameStuff").slideUp(settings.animationSpeed,settings.easing);
+			numGuesses=0;
+			$(".numOfGuesses").text(numGuesses);
+			$("#guess #tag").val("").focus();
+
+			currentTag = utilities.getTag();
+			//get JSON data from Flickr
+			utilities.getImages(currentTag.word);
+		},
+		guess:function(){
+			guessedTag=$("#guess #tag").val();
+			if($.trim(guessedTag) != ""){
+				numGuesses++;
+				$(".numOfGuesses").text(numGuesses);
+				if(guessedTag==currentTag.word){
+					//Correct!
+					actions.winGame();
+				}else{
+					//Wrong!
+					if(numGuesses==1) $("#gameStuff").slideDown(settings.animationSpeed,settings.easing);
+					$("#guess #tag").effect("highlight",{"color":"#F00F00"},settings.animationSpeed).val("").focus();
+					$("#guesses .clear").before("<li>"+guessedTag+"</li>")
+					$("#guesses :nth-child("+numGuesses+")").hide().show(settings.animationSpeed,settings.easing);
+				}
+			}
+		},
+		winGame:function(){
+			timer.stop();
+			$('#winDialog').dialog('open');
+		},
+		loseGame:function(){
+			timer.stop();
+			$("#giveUpTag").text(currentTag.word);
+			$('#loseDialog').dialog('open');
+
+			return false;
+		},
+		displayHint:function(){
+			var hintText = "The word is <strong>" + currentTag.categoryName + "</strong> and it has <strong>"+ currentTag.word.length + "</strong> letters."
+			$("#hintText").html(hintText);
+			$("#hint").show();
+		}
+	};
+
+	var utilities = {
+		getRandomFromArray:function(arr){
+			return arr[Math.floor(Math.random() * arr.length)];
+		},
+		getTag:function(){
+			var category = utilities.getRandomFromArray($Game.Words);
+
+			return{
+				categoryName: category.categoryName,
+				word: utilities.getRandomFromArray(category.wordList)
+			}
+		},
+		getImages:function(tag){
+			var loadedImgs=0;
+
+			var getImagesUrl = "http://api.flickr.com/services/feeds/photos_public.gne?tags="+tag+"&tagmode=any&format=json&jsoncallback=?";
+			$.getJSON(getImagesUrl,function(data){
+				$.each(data.items, function(i,item){
+					var image_medium = item.media.m;
+					var image_large = image_medium.replace(/_m\.jpg/g,'.jpg');
+					var image_small = image_medium.replace(/_m\.jpg/g,'_s.jpg');
+
+					//Pre-load all the large images
+					$("<img src='"+image_large+"' />").appendTo("#preloadPhotos");
+
+					//Add the small images to the grid
+					$("<img alt='' src='"+image_small+"'/>")
+						.appendTo("#photos")
+						.load(function(){
+							$(this).wrap("<a href='"+image_large+"'></a>");
+							$("#photos a").fancybox({
+								'zoomSpeedIn':settings.animationSpeed,
+								'zoomSpeedOut':settings.animationSpeed,
+								'overlayShow': false,
+								'hideOnContentClick':true
+							});
+							loadedImgs++;
+							if(loadedImgs == settings.imagesToDisplay){
+								//start timer once all images are loaded
+								timer.start();
+								loadedImgs=0;
+							}
+						});
+
+					//Stop when we get enough images
+					if (i == settings.imagesToDisplay-1) return false;
+				});
+			});
+		}
+	}
+
+	var timer={
+		interval: null,
+		currentTime:0,
+		start:function(){
+			timer.currentTime = settings.gameDuration + 1;
+			timer.tick();
+		},
+		tick:function(){
+			timer.currentTime--;
+			timer.update(timer.currentTime);
+			if(timer.currentTime<=0){
+				return actions.loseGame();
+			}else{
+
+				if(timer.currentTime === Math.round(settings.gameDuration/2)){
+					actions.displayHint();
+				}
+
+				if(timer.currentTime === 10){
+					$("#timer").animate({color:"#FF0000"},settings.animationSpeed);
+				}
+				timer.timeout = setTimeout(timer.tick,1000);
+			}
+		},
+		stop:function(){
+			clearTimeout(timer.timeout);
+			timer.update(0);
+			$("#timer").removeAttr("style");
+		},
+		update:function(time){
+			var displayTime=time;
+			if(time<10) displayTime="0"+time;
+			$("#timer").text(":"+displayTime);
+		}
+	};
+
+	//Need a return so we can extend this with the word list in another file
+	return{};
+
+}(jQuery);
---
+++ js/words.js
@@ -0,0 +1,36 @@
+//var words = ['air','arms','army','art','audience','ball','bed','black','blood','blue','board','body','book','boys','bring','british','building','car','change','chief','child','children','church','city','class','club','cold','college','color','congress','cut','dark','data','day','dead','degree','design','door','drive','earth','east','eight','element','england','english','europe','eye','face','family','farm','father','feet','fire','food','foot','four','french','game','girl','green','ground','group','grow','hair','half','hall','hand','head','heart','hell','horse','hospital','hot','hotel','hour','house','human','hundred','husband','idea','island','letter','level','light','lines','list','little','live','love','low','man','march','market','married','mass','medical','meeting','military','money','mother','music','natural','nature','north','note','nuclear','numbers','off','office','out','outside','paper','parts','party','pattern','people','plane','plant','play','police','pool','present','press','public','race','radio','range','read','record','red','river','road','room','running','saw','school','science','second','seven','short','shot','six','south','soviet','space','spring','square','stage','stop','story','street','strong','student','sun','table','ten','things','third','thirty','three','time','town','twenty','two','unicorn','volume','wall','water','west','wife','window','woman','year','zebra','zipper','africa','animals','architecture','art','asia','australia','autumn','baby','band','barcelona','beach','berlin','bird','birthday','california','canada','cat','christmas','clouds','concert','dance','dog','film','florida','flower','football','france','garden','graffiti','halloween','hawaii','hiking','kids','light','live','mountain','ocean','rock','sea','sky','snow','sunset','tree','wedding','white','yellow','zoo'];
+
+$Game.Words = [
+	{
+		categoryName:'a noun',
+		wordList:['arm','army','art','ball','bed','black','blood','blue','body','book','boy','building','car','change','chief','child','church','city','class','club','college','congress','dead','door','drive','earth','eye','face','farm','feet','fire','food','foot','girl','green','ground','grow','hair','hand','head','heart','hell','hospital','hotel','house','island','letter','man','march','market','military','money','note','nuclear','numbers','off','office','out','paper','party','pattern','plane','plant','police','pool','race','radio','record','river','road','room','saw','school','space','square','street','sun','table','wall','water','window','woman','zipper','baby','band','beach','christmas','cloud','concert','flower','football','garden','graffiti','mountain','ocean','rock','sea','sky','snow','sunset','tree','wedding','white','wheel']
+	},
+	{
+		categoryName:'a verb',
+		wordList:['run','sit','stand','catch','throw','walk','fly','swim','play']
+	},
+	{
+		categoryName:'an adjective',
+		wordList:['ravishing','outstanding','mimic','famous','cheerful','livid','obstinate','exhausted','graceful','outrageous','radical','childish','snobbish','miserly','amiable','disgusting','awful','humorous','fanciful','pathetic','windy','dusty','bashful','freaky','chilly','stormy','humid','bountiful','jubilant','irritated','patient','dizzy','skeptical','puzzled','light-hearted','perplexed','overwhelmed','jovial','hyper','squirrelly','jittery','sensational','elegant','gleeful','flabbergasted','dreary','impish','sneaky','horrid','monstrous']
+	},
+	{
+		categoryName:'a description',
+		wordList:['angry','brave','bright','busy','clever','cold','cozy','deep','flat','foggy','free','fresh','frozen','gentle','giant','glad','grand','hollow','hungry','hurt','lucky','neat','new','old','polite','proud','rough','serious','shiny','short','shy','smooth','spotted','strong','tall','tough','weak','wide','wild','wise','bumpy','careful','chilly','clean','cloudy','crisp','damp','enormous','fancy','flashy','flowery','frosty','fuzzy','huge','icy','kind','marvelous','merry','messy','mighty','misty','moldy','plaid','plain','quiet','scented','selfish','sharp','slim','slippery','sloppy','sly','soggy','spicy','stormy','striped','sweet','tasty','tiny','velvety','twinkling','weak','worn','young']
+	},
+	{
+		categoryName:'an animal',
+		wordList:['Ant','Antelope','Ape','Ass','Badger','Bat','Bear','Beaver','Bird','Boar','Camel','Canary','Cat','Cheetah','Chicken','Chimpanzee','Chipmunk','Cow','Crab','Crocodile','Deer','Dog','Dolphin','Donkey','Duck','Eagle','Elephant','Ferret','Fish','Fox','Frog','Goat','Hamster','Hare','Horse','Kangaroo','Leopard','Lion','Lizard','Mole','Monkey','Mule','Ostrich','Otter','Panda','Parrot','Pig','Polecat','Porcupine','Rabbit','Rat','Rhinoceros','Seal','Sheep','Snake','Squirrel','Tapir','Toad','Tiger','Tortoise','unicorn','Walrus','Whale','Wolf','Zebra']
+	},
+	{
+		categoryName:'a color',
+		wordList:['yellow','blue','red','pink','green','black','white','purple','teal','silver','tan','grey','gold','brown','cyan','orange']
+	},
+	{
+		categoryName:'a number',
+		wordList:['one','two','three','four','five','six','seven','eight','nine','ten']
+	},
+	{
+		categoryName:'a direction',
+		wordList:['up','down','left','right','east','west','north','south']
+	}	
+];
